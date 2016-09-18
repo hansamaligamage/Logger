19bb0b820460f9493c9379eef7f832b3109de1b7
hansamali <hansamali.gamage@gmail.com>
2016-09-18 22:36:11 +0530

    Add files via upload

---
+++ NLogConfiguration/NLogConfiguration/Program.cs
@@ -0,0 +1,24 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = LogManager.GetCurrentClassLogger();
+            logger.Trace("trace message");
+            logger.Debug("debug message");
+            logger.Info("info message");
+            logger.Warn("warn message");
+            logger.Error("error message");
+            logger.Fatal("fatal message");
+            logger.Log(LogLevel.Info, "log message");
+        }
+    }
+}
